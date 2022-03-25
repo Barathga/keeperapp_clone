627624b1e105c6a3ebc41ca302489c9f37f1b0fc
barathga <barathga1999@gmail.com>
2022-03-25 11:52:09 +0530

    git branch -M master#

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components.js/Header";
+import Footer from "./Components.js/Footer";
+// import Body from "./Components.js/Body";
+import Notes from "./Components.js/Notes";
+import Createnotes from "./Components.js/Createnotes";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newnote) {
+    setNotes((prevNote) => {
+      return [...prevNote,newnote];
+    });
+  }
+
+  function deletenote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id ;
+      })
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Createnotes onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return <Notes key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deletenote}/>;
+      })}
+      <Footer />
     </div>
   );
 }
